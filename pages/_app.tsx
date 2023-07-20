00241fff84bcd4259e97e456957942fc55693aee
Prathamesh48 <prathameshpatil6499@gmail.com>
2023-07-20 19:06:09 +0530

    app dir

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { polygonMumbai } from "wagmi/chains";
+import { Toaster } from "react-hot-toast";
+
+const chains = [polygonMumbai];
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: "2yLPbaXyrARDaJPk4lOe-f8D4ceX-j1P", // or infuraId
+    walletConnectProjectId: "a0c93d8a0ad928d0ca69ad15058d48a3",
+    chains,
+    // Required
+    appName: "BrewBucks",
+  })
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider
+        debugMode
+        theme="soft"
+        customTheme={{ "--ck-connectbutton-border-radius": "3rem" }}
+      >
+        <RecoilRoot>
+          <Toaster position="bottom-right" />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
+
