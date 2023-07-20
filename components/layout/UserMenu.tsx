517db1bd8d2d106f247c78613a1f39e02034254c
Prathamesh48 <prathameshpatil6499@gmail.com>
2023-07-20 23:52:26 +0530

    usermenu done

--- components/layout/UserMenu.tsx
+++ components/layout/UserMenu.tsx
@@ -27,7 +27,7 @@ const UserMenu = () => {
   return (
     <>
       {currentUser.hasProfile && <ConnectKitButton />}
-      <Menu as="div" className="relative inline-block text-left">
+      <Menu as="div" className="relative inline-block text-left w-full">
         {currentUser.hasProfile ? (
           <Menu.Button className="dark-blue flex relative gap-2 rounded-lg py-1 px-2 pl-3 ml-4 ">
             {currentUser?.profile?.username}
@@ -41,8 +41,8 @@ const UserMenu = () => {
           <div className="p-1 text-left flex flex-col min-w-fit gap-1">
             {currentUser.hasProfile && (
               <Menu.Item>
-                <Link href={`/u/${currentUser?.profile?.username}`}>
-                  <p className="px-4 py-1 whitespace-nowrap font-display hover:bg-slate-700 rounded-lg">
+                <Link href={`/${currentUser?.profile?.username}`}>
+                  <p className="px-4 py-1 whitespace-nowrap font-display hover:bg-[#cee4fe] rounded-md text-gray-500 hover:text-gray-700">
                     My Channel
                   </p>
                 </Link>
@@ -51,17 +51,17 @@ const UserMenu = () => {
             {currentUser.hasProfile && (
               <Menu.Item>
                 <Link href={`/dashboard`}>
-                  <p className="px-4 py-1 whitespace-nowrap font-display text-left hover:bg-slate-700 rounded-lg">
+                  <p className="px-4 py-1 whitespace-nowrap font-display text-left hover:bg-[#cee4fe] rounded-md text-gray-500 hover:text-gray-700">
                     Dashboard
                   </p>
                 </Link>
               </Menu.Item>
             )}
-            <hr className=" border-gray-600" />
+            <hr className=" border border-[#0072f5]" />
             <Menu.Item>
               <button
                 onClick={handleDisconnect}
-                className=" hover:bg-slate-700 px-4 py-1 w-full block text-left rounded-lg font-normal"
+                className="text-black px-4 py-1 w-full block text-left rounded-md font-normal hover:bg-red-500 hover:text-white"
               >
                 Disconnect{" "}
               </button>
